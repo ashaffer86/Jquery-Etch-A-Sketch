df4bdc67caa15b2c6ca0fe5cf071949a8ae0e682
ashaffermyers <andrewshaffer1986@gmail.com>
2017-02-07 20:57:55 -0600

    boxes are now filled in

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 
     createGrid(5,5);
-
-  /*$("#grid").mouseOver(); */
+    $("#grid div > div").mouseover(function(){
+      $(this).addClass("blue");
+    });
 });
 
 function createGrid(rows, columns){
@@ -13,10 +14,12 @@ function createGrid(rows, columns){
     gridString += "<div id='row" + (r+1) + "'>";
 
     for(c = 0; c < columns; c++){
-      gridString += "<div id='column" + (c+1) + "' class='box'>Hello World " + (c+1) + "</div>";
+      gridString += "<div id='item" + (r+1) + "" + (c+1) + "'>Hello World " + (c+1) + "</div>";
     }
     gridString += "</div>";
 
   }
   $("#grid").append(gridString);
+  $("#grid div > div").css({"width":"" + 50 + "px","height":"" + 50  + "px"});
+  /*try next time : 80.0/rows */
 }
