ec180576c6968b0850e3720352637116a0d77d34
ashaffermyers <andrewshaffer1986@gmail.com>
2017-02-09 20:55:23 -0600

    centered

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
     createGrid(5,5);
     $("#grid div > div").mouseover(function(){
-      $(this).addClass("blue");
-    });
+      $(this).addClass("blue")});
+
 });
 
 function createGrid(rows, columns){
@@ -14,12 +14,16 @@ function createGrid(rows, columns){
     gridString += "<div id='row" + (r+1) + "'>";
 
     for(c = 0; c < columns; c++){
-      gridString += "<div id='item" + (r+1) + "" + (c+1) + "'>Hello World " + (c+1) + "</div>";
+      gridString += "<div id='item" + (r+1) + "" + (c+1) + "'>Hello World " + (r+1) + "" + (c+1)  + "</div>";
     }
     gridString += "</div>";
 
   }
   $("#grid").append(gridString);
-  $("#grid div > div").css({"width":"" + 50 + "px","height":"" + 50  + "px"});
+  $("#grid div > div").css({"width":"" + 900.0/columns + "px","height":"" + 900.0/rows + "px"});
   /*try next time : 80.0/rows */
+}
+
+function addblue(i){
+    $(i).addClass("blue");
 }
